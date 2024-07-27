7ab5c05aaeb2fa158947e9578b978cd0e83ec8ba
Xuanwo <github@xuanwo.io>
2024-07-27 18:19:48 +0800

    Format python
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- tests/test_opendalfs.py
+++ tests/test_opendalfs.py
@@ -3,8 +3,6 @@ from opendalfs_service_memory import MemoryFileSystem
 from opendalfs_service_s3 import S3FileSystem
 
 
-
-
 def test_memory_fs():
     # Default
     MemoryFileSystem()
@@ -19,10 +17,10 @@ def test_s3_fs():
     S3FileSystem(root="/tmp", bucket="test", region="us-east-1")
 
 
-
 @pytest.fixture
 def opendal_fs():
     return MemoryFileSystem(root="/tmp")
+
 
 # TODO: we need to find a way to make it work
 # def test_inheritance(opendal_fs):
