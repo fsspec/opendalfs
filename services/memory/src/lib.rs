e1d66b76772c16a2bd90304e65b12cdd533dcea3
Xuanwo <github@xuanwo.io>
2024-07-27 18:18:35 +0800

    feat: Add s3 support
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- services/memory/src/lib.rs
+++ services/memory/src/lib.rs
@@ -2,32 +2,24 @@ use opendal::services::Memory;
 use opendal::Operator;
 use opendalfs::OpendalFileSystem;
 use pyo3::prelude::*;
-use pyo3::types::PyDict;
 
+#[derive(Default)]
 #[pyclass(extends=OpendalFileSystem)]
-pub struct MemoryFileSystem;
-
-#[derive(Default, Clone, FromPyObject)]
-#[pyo3(from_item_all)]
-pub struct MemoryConfig {
+pub struct MemoryFileSystem {
     root: Option<String>,
 }
 
 #[pymethods]
 impl MemoryFileSystem {
     #[new]
-    #[pyo3(signature = (**kwargs))]
-    pub fn new(kwargs: Option<&Bound<'_, PyDict>>) -> (Self, OpendalFileSystem) {
-        let cfg = kwargs
-            .map(|arg| MemoryConfig::extract_bound(arg.as_any()))
-            .transpose()
-            .unwrap()
-            .unwrap_or_default();
+    #[pyo3(signature = (root=None))]
+    pub fn new(root: Option<String>) -> (Self, OpendalFileSystem) {
+        let cfg = MemoryFileSystem { root };
 
         let mut builder = Memory::default();
-        builder.root(&cfg.root.unwrap_or_default());
+        builder.root(&cfg.root.clone().unwrap_or_default());
         let op = Operator::new(builder).unwrap().finish();
-        (MemoryFileSystem, OpendalFileSystem::from(op))
+        (cfg, OpendalFileSystem::from(op))
     }
 }
 
