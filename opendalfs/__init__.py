5c74dd7dbb8580bc8118afa9e47b9288a57b3afa
Xuanwo <github@xuanwo.io>
2025-12-24 23:50:04 +0800

    feat: register URL Protocols (#30)

--- opendalfs/__init__.py
+++ opendalfs/__init__.py
@@ -1,3 +1,8 @@
 from .fs import OpendalFileSystem
+from .registry import register_opendal_protocols, register_opendal_service
 
-__all__ = ["OpendalFileSystem"]
+__all__ = [
+    "OpendalFileSystem",
+    "register_opendal_protocols",
+    "register_opendal_service",
+]
